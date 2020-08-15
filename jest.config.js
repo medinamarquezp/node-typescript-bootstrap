b95f71d8d6bf98b9b6d72e57017569057380a954
Pedro Medina <medinamarquezp@gmail.com>
2020-08-15 20:19:33 +0200

    Initial configuration

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: "node",
+  roots: ["<rootDir>"],
+  transform: {
+    "^.+\\.ts?$": "ts-jest"
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  moduleNameMapper: {
+    "^@(.*)$": "<rootDir>/src/$1"
+  }
+};
